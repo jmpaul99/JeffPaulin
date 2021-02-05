6cfb089ba71e39c798af68710101337a8884b70f
Jeff Paulin <jmpaulin13@gmail.com>
2021-02-04 23:01:25 -0500

    Embeded Power BI report as quick fix to show coaches session notes

---
+++ Controllers/SessionsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JeffPaulin.Controllers
+{
+    public class SessionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
